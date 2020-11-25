f19dde5e38016a60208b5f182b416b521790d0a2
acabezar <uzumaki1995>
2020-11-25 14:44:05 +0100

    changes

--- src/app/@pages/about/about.module.ts
+++ src/app/@pages/about/about.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
-import { SharedModule } from "../../shared/shared.module";
 import { AboutDetailComponent } from './pages/about-detail/about-detail.component';
 
 
